d6f7325b38c5ff2749d1d144f3844b599ca8f4b7
bmurdock <brian.j.murdock@gmail.com>
2021-05-07 19:50:56 -0600

    first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const baseRoute = `https://pokeapi.co/api/v2`;
+
+
+class App extends React.Component {
+  constructor()
+  {
+    super();
+
+    console.log('this happens in the constructor');
+
+    this.state = {
+      pokemonName: 'Brian',
+      clicks: 0,
+    }
+  }
+
+  incrementClicks = () =>
+  {
+    this.setState({
+      clicks: this.state.clicks + 1,
+    });
+  }
+
+  componentDidMount()
+  {
+    console.log('Mounting');
+    fetch(`${baseRoute}/pokemon/1`)
+    .then((response) =>
+    {
+      // error handling and stuff
+      if (response.status === 200)
+      {
+        return response.json();
+      }
+      throw new Error(`Could not get pokemon: ${response}`);
+    })
+    .then((pokemon) =>
+    {
+      console.log(pokemon);
+      this.setState({
+        pokemonName: pokemon.name,
+      });
+    })
+    .catch((oops) =>
+    {
+      console.error(`Uh oh, we could not fetch the pokemon: ${oops}`);
+    })
+  }
+
+  render()
+  {
+    console.log(`it's rendering...`);
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <p>
+            Name: {this.state.pokemonName}
+          </p>
+          <p>You have clicked {this.state.clicks} times.</p>
+          <button onClick={this.incrementClicks}>Click Me</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
